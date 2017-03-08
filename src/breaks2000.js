60ec0c9049eec1c588ef0553f09c2f425dda0bcc
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2017-03-08 19:30:23 +0100

    Added umd build and test npm script

--- src/breaks2000.js
+++ src/breaks2000.js
@@ -2,54 +2,54 @@
  * Resize handler. Calculate widths and add classes.
  */
 function update() {
-  console.time('onResize');
+  console.time('onResize')
 
-  var elements = document.querySelectorAll('[data-breaks]'),
-    element,
-    classes,
-    elementWidth,
-    breakPoints,
-    breakPoint;
+  var elements = document.querySelectorAll('[data-breaks]')
+  var element
+  var classes
+  var elementWidth
+  var breakPoints
+  var breakPoint
 
   for (var i = 0, l = elements.length; i < l; i++) {
-    element = elements[i];
-    classes = element.className;
-    elementWidth = element.offsetWidth;
-    breakPoints = element.getAttribute('data-breaks').split(',');
-    classes = classes.replace(/\s*[<>]\d+/g, '');
+    element = elements[i]
+    classes = element.className
+    elementWidth = element.offsetWidth
+    breakPoints = element.getAttribute('data-breaks').split(',')
+    classes = classes.replace(/\s*[<>]\d+/g, '')
     for (var j = 0, k = breakPoints.length; j < k; j++) {
-      breakPoint = breakPoints[j];
+      breakPoint = breakPoints[j]
       if (__MOBILE_FIRST__) {
         if (elementWidth >= +breakPoint) {
-          classes += ' >' + breakPoint;
+          classes += ' >' + breakPoint
         } else {
-          break;
+          break
         }
       } else {
         if (elementWidth <= +breakPoint) {
-          classes += ' <' + breakPoint;
+          classes += ' <' + breakPoint
         }
       }
     }
-    element.className = classes;
+    element.className = classes
   }
 
-  console.timeEnd('onResize');
-};
+  console.timeEnd('onResize')
+}
 
 /**
  * Initialize responsive elements. Called once on page load.
  */
 function init() {
-  update();
+  update()
   if (__IE8_SUPPORT__) {
     if (window.addEventListener) {
-      window.addEventListener('resize', update, false);
+      window.addEventListener('resize', update, false)
     } else {
-      window.attachEvent('onresize', update);
+      window.attachEvent('onresize', update)
     }
   } else {
-    window.addEventListener('resize', update, false);
+    window.addEventListener('resize', update, false)
   }
 };
 
@@ -59,21 +59,21 @@ function init() {
 function uninit() {
   if (__IE8_SUPPORT__) {
     if (window.removeEventListener) {
-      window.removeEventListener('resize', update, false);
+      window.removeEventListener('resize', update, false)
     } else {
-      window.detachEvent('onresize', update);
+      window.detachEvent('onresize', update)
     }
   } else {
-    window.removeEventListener('resize', update, false);
+    window.removeEventListener('resize', update, false)
   }
 
-  var elements = document.querySelectorAll('[data-breaks]'),
-    element;
+  const elements = document.querySelectorAll('[data-breaks]')
+  let element
 
   for (var i = 0, l = elements.length; i < l; i++) {
-    element = elements[i];
-    element.className = element.className.replace(/\s*[<>]\d+/g, '');
+    element = elements[i]
+    element.className = element.className.replace(/\s*[<>]\d+/g, '')
   }
-};
+}
 
-export { init, uninit, update };
+export { init, uninit, update }
